b4b09a59500d7bf587d3147c0c873fdb6b7285a4
alexadra16 <alexandrafernandezr20@gmail.com>
2020-02-26 10:12:36 -0500

    isValid, maskify y test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 import validator from './validator.js';
+// console.log(validator.isValid('12345'));
+// console.log(maskify);
+// validator.maskify
+const validat1= document.getElementById("validarNumTarj");
+const validat0= document.getElementById("num");
 
-console.log(validator);
+    // console.log(typeof validat1);
+    // console.log(validat1);
+
+    validat1.addEventListener("click", ()=>{
+        document.querySelector("#resul").style.backgraoundColor = "gray";
+        let validadorfinal= document.getElementById("resul");
+        validadorfinal.innerHTML = validator.isValid(validat0.value)
+        validat0.value = validator.maskify(validat0.value)
+    })
+    // console.log(validat1)
+
+    
+
+
+   
+    //validat0.addEventListener("keyup", ()=>{
+    // let validadorfinal2= document.getElementById("validator");
+        //validat0.value = validator.maskify(validat0.value)
+
+    //})
+    //console.log(validat0)
+//detectar los numeros que estan en posicion par 
+//const sum = 0;
+//for(let i=0; i<invertido.length; i++){
+//    let tmp=carray[i];
+//    if((i % 2) !=0) {
+//        tmp*=2;
+//        if(tmp>9){
+//            tmp-=9;
+//        }
+//    }
+//    sum+=tmp;
+//}
+//return (sum%10)== 0;
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,50 @@
 const validator = {
-  // ...
-};
+ isValid:(a)=>{
+    let numTarjeta = a.split("");
+    // console.log(a);
+
+    const  numTarjetaReverse = numTarjeta.reverse();
+    let sumPosicionPar = 0;
+    let sumPosicionImpar = 0;
+    let valor = true;
+    
+    for(let i = 1; i < numTarjetaReverse.length; i = i + 2) {
+      const resultAfter2=numTarjetaReverse[i]*2
+      if(resultAfter2>9){
+        const resultAfter10 = ((resultAfter2 % 10) + 1)
+        sumPosicionPar = sumPosicionPar + resultAfter10
+        //console.log(resultAfter10)
+      } else {
+        sumPosicionPar = sumPosicionPar + resultAfter2
+        //console.log(resultAfter2)
+      }
+    }
+    //console.log(sumPosicionPar)
+    for(let i = 0; i < numTarjetaReverse.length; i= i + 2) {
+      const resultAfterImpares = numTarjetaReverse[i]*1;
+    sumPosicionImpar = sumPosicionImpar + resultAfterImpares
+    
+    }
+    // console.log(sumPosicionImpar);
+    const sumTotal=sumPosicionImpar + sumPosicionPar
+    if(sumTotal%10 == 0){
+      valor = true;
+    } else {
+      valor = false;
+    }
+    return valor;
+ },
+//  console.log(isValid("4083952015263"));
+ maskify:(b) =>{
+    let cantidad= b.length;
+    let output = "";
+    for(let i =0;i<cantidad-4;i++)
+    {
+      output+="#";
+    }
+    return output + b.substring(cantidad -4,cantidad);
+ }
+//  console.log(maskify("4083952015263"));
+}
 
 export default validator;
